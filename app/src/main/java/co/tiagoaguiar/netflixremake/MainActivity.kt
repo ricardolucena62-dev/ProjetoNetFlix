34fb750f44d8aae434bf254229217bba9aea9701
ricardolucena62-dev <ricardolucena62@gmail.com>
2026-04-05 21:41:50 -0300

    Segundo commit

--- app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
@@ -7,27 +7,21 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import co.tiagoaguiar.netflixremake.model.Category
 import co.tiagoaguiar.netflixremake.model.Movie
+import co.tiagoaguiar.netflixremake.util.CategoryTask
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("Teste", "onCreat")
 
         val categories = mutableListOf<Category>()
-        for(j in 0 until 5){
-            val movies = mutableListOf<Movie>()
-            for(i in 0 until 15){
-                val movie = Movie(R.drawable.movie)
-                movies.add(movie)
-            }
-            val category = Category("cat $j", movies)
-            categories.add(category)
-        }
 
         val adapter = CategoryAdapter(categories)
         val rv : RecyclerView = findViewById(R.id.rv_main)
         rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
+
+
+
     }
 }
