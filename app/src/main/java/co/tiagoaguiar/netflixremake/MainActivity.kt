1d5ca35d16227d60cda1bbad9239ae334ce26162
ricardolucena62-dev <ricardolucena62@gmail.com>
2026-04-10 15:11:40 -0300

    Terminando as configurações de API, implemnetando tratamento  de fechamento de funções (buffer e stream) no try catch, Criamos uma interface na categoryTask para executar funções  do progressBar e instamciamos a CategoryTask na Main.

--- app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
@@ -3,16 +3,23 @@ package co.tiagoaguiar.netflixremake
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import co.tiagoaguiar.netflixremake.model.Category
 import co.tiagoaguiar.netflixremake.model.Movie
 import co.tiagoaguiar.netflixremake.util.CategoryTask
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),CategoryTask.Callback {
+
+    lateinit var progress : ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progress = findViewById(R.id.progress_main)
 
         val categories = mutableListOf<Category>()
 
@@ -20,8 +27,22 @@ class MainActivity : AppCompatActivity() {
         val rv : RecyclerView = findViewById(R.id.rv_main)
         rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
+        CategoryTask(this).execute("https://atway.tiagoaguiar.co/fenix/netflixapp/home?apiKey=034f17ec-8079-403c-b55a-5286bfd46b53")
+    }
 
+    override fun onPreExecute() {
+        progress.visibility= View.VISIBLE
+    }
 
+    override fun onResult(categories: List<Category>) {
+        //Aqui será quando o categoryTask chamar de volta.
+        //Callback - listener
+        Log.i("Teste MainActiviti", categories.toString())
+        progress.visibility= View.GONE
+    }
 
+    override fun onFailure(message: String) {
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+        progress.visibility= View.GONE
     }
 }
--- app/src/main/java/co/tiagoaguiar/netflixremake/util/CategoryTask.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/util/CategoryTask.kt
@@ -1,37 +1,101 @@
 package co.tiagoaguiar.netflixremake.util
 
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import co.tiagoaguiar.netflixremake.model.Category
+import co.tiagoaguiar.netflixremake.model.Movie
+import org.json.JSONObject
+import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
 import java.io.IOException
+import java.io.InputStream
 import java.net.URL
 import java.util.concurrent.Executors
 import javax.net.ssl.HttpsURLConnection
 
-class CategoryTask {
-     fun execute(url:String){
+class CategoryTask(private val callback : Callback) {
 
+    private val handler = Handler(Looper.getMainLooper())
+    interface Callback{
+    fun onPreExecute()
+    fun onResult(categories:List<Category>)
+    fun onFailure(message : String)
+
+    }
+     fun execute(url:String){
+         callback.onPreExecute()
          //Ultilizando a UI-thread(1)
          val executor = Executors.newSingleThreadExecutor()
 
          //Ultilizando a nova UI-Thread em (processo paralelo) (2)
          executor.execute{
+             var urlConection: HttpsURLConnection? = null
+             var buffer : BufferedInputStream? = null
+             var stream : InputStream? = null
              try {
                  val requestURL = URL(url)
-                 val urlConection = requestURL.openConnection() as HttpsURLConnection // abrir a conexão.
+                 urlConection = requestURL.openConnection() as HttpsURLConnection // abrir a conexão.
                  urlConection.readTimeout = 2000 // tempo de leitura
                  urlConection.connectTimeout = 2000 // tempo de conexão
                  val statusCode: Int = urlConection.responseCode
                  if (statusCode > 400){
                     throw IOException("Erro na comunicação com o servidor.")
                  }
-                 val stream = urlConection.inputStream // sequencia de bytes
-                 val jsonAsString = stream.bufferedReader().use { it.readText() } // bytes -> String
-                 Log.i("Teste", jsonAsString)
+                 stream = urlConection.inputStream // sequencia de bytes
+                 buffer = BufferedInputStream(stream) // Recebe o arquivo json
+                 val jsonAsString = toString(buffer)
+                 val categories = toCategories(jsonAsString)
+                 handler.post {
+                     callback.onResult(categories) // Aqui roda dentro a UI- thread
+                 }
+
 
             }catch(e:IOException){
-                Log.e("Teste", e.message ?: "Erro desconhecido.",e)
-            }
-
+                val message = e.message ?: "Erro desconhecido."
+                Log.e("Teste", e.message, e)
+                 handler.post {
+                     callback.onFailure(message)
+                 }
+            } finally {
+                urlConection?.disconnect()
+                 stream?.close()
+                 buffer?.close()
+             }
          }
-
      }
+    private fun toCategories(jsonAsString: String): List<Category>{
+        val categories = mutableListOf<Category>()
+        val jsonRoot = JSONObject(jsonAsString)
+        val jsonCategories= jsonRoot.getJSONArray("Category")
+        for(i in 0 until jsonCategories.length()){
+            val jsonCAtegory = jsonCategories.getJSONObject(i)
+            val title = jsonCAtegory.getString("title")
+            val jsonMovies = jsonCAtegory.getJSONArray("movie")
+            val movies = mutableListOf<Movie>()
+            for(j in  0 until jsonMovies.length()){
+                val jsonMovie = jsonMovies.getJSONObject(j)
+                val id = jsonMovie.getInt("id")
+                val coverUrl = jsonMovie.getString("cover_url")
+                movies.add(Movie(id,coverUrl))
+            }
+         categories.add(Category(title,movies))
+        }
+        return categories
+    }
+
+    private fun toString(stream : InputStream) : String {
+        val bytes = ByteArray(1024)
+        val baos =ByteArrayOutputStream()
+        var read : Int
+        while (true){
+            read = stream.read(bytes)
+            if(read <= 0){
+                break
+            }
+            baos.write(bytes , 0 , read) // Escreva os bytes novos de zero ate onde read conseguir ler.
+        }
+        return String(baos.toByteArray())
+    }
+
 }
